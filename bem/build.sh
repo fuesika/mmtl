d655b7937c83c60a1a594479f0e984a084d65f34
fuesika <github@fuesika.de>
2015-02-17 10:35:08 -0800

    convert build process of bem to be based on cmake

---
+++ bem/build.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+## configure ###################################################################
+#get CMAKE_INSTALL_PREFIX
+ROOTDIR="${PWD}"
+#set CMAKE_INSTALL_PREFIX
+INST_PRFX=''
+
+NCORES=$(grep -ic ^processor /proc/cpuinfo)
+
+## Build MLSS from scratch ###################################################
+#clean, create and cd to build-directory
+echo "== Configure MMTL-BEM ================================================="
+echo "Delete old build-dir and binaries..";
+cd "${ROOTDIR}"
+[ -d "${ROOTDIR}/build" ] && rm -rIf "${ROOTDIR}/build"
+rm -rIf "${ROOTDIR}/bin"     &> /dev/null
+mkdir build
+cd build
+echo "[done]";
+
+#start cmake
+echo "Run cmake..";
+cmake ../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INST_PRFX
+
+## make MMTL-BEM ###############################################################
+echo "== Build MMTL-BEM ====================================================="
+echo -n "Build MMTL-BEM now? ([Y]/N)"
+read CONFIRM
+case $CONFIRM in
+  n|N|NO|no|No) echo 'Not building MMTL-BEM. Please cd to build-directory and run "make" and possibly "make install" for manual handling.'
+    ;;
+  *) cd $ROOTDIR/build
+    make -j $NCORES
+    ;;
+esac
+
+#install mlss{,-dev} to binary-directory
+make DESTDIR="$ROOTDIR" install
