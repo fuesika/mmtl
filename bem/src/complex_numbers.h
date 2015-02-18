84549405cc558b8c3c29cabc755ab6636257a65c
fuesika <github@fuesika.de>
2015-02-17 21:55:35 -0800

    minor refactoring, removal of orphaned statements

--- bem/src/complex_numbers.h
+++ bem/src/complex_numbers.h
@@ -3,45 +3,18 @@
 
 /* definitions required for use of complex numbers */
 
-typedef struct 
-{
-  float real;                    /* the real part of the complex number */
-  float imaginary;		/* the imaginary part of hte complex number */
+typedef struct {
+  //real part
+  float real;         
+  //imaginary part           
+  float imag;		
 } COMPLEX;
 
-typedef struct
-{
-  double real;                    /* the real part of the complex number */
-  double imaginary;                   /* the imaginary part of the
-				     complex number 
-				  */
+typedef struct {
+  //real part
+  double real;     
+  //imaginary part               
+  double imag;
 } DOUBLE_COMPLEX;
-
-/* these are the prototypes for the functions that perform
-*  operations on the complex numbers
-*  using these requires that the object be linked against
-*  complex_numbers.c
-*/
-
-/* make a complex number from two floats*/
-COMPLEX cplx(double x,double y);
-
-/* the square root of a complex number */
-COMPLEX csqrt(COMPLEX);
-
-/* the complex exponential */
-COMPLEX cexp(COMPLEX);
-
-/* divide  the first number by the second */
-COMPLEX cdiv(COMPLEX,COMPLEX);
-
-/* multiply  two complex numbers */
-COMPLEX cmult(COMPLEX,COMPLEX);
-
-/* find the absolute value of a complex number */
-float cmag(COMPLEX);
-
-/* find the complex conjagate of a complex number */
-COMPLEX conj(COMPLEX);
 
 #endif
