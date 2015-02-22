c966d2e71c12b00a09717e6e3f10aaeb7528447f
fuesika <github@fuesika.de>
2015-02-21 20:58:41 -0800

    fix Wmaybe-uninitialized

--- bem/src/nmmtl_dc_resistance.cpp
+++ bem/src/nmmtl_dc_resistance.cpp
@@ -19,7 +19,7 @@ static double nmmtlPolyArea(CONTOURS_P item)
   double x0;
   double y0;
 
-  area = 0;
+  area = 0.;
   point = item->points;
   x0 = point->x;
   y0 = point->y;
@@ -34,11 +34,10 @@ static double nmmtlPolyArea(CONTOURS_P item)
 }
 
 void nmmtl_dc_resistance(double the_conductivity,
-       CONTOURS_P signals,
-       double **Rdc,
-       FILE *fp1,
-       FILE *fp2)
-{
+                         CONTOURS_P signals,
+                         double **Rdc,
+                         FILE *fp1,
+                         FILE *fp2) {
   CONTOURS_P item;
   CONTOURS_P sig_line1;
   CONTOURS_P sig_line2;;
@@ -59,57 +58,55 @@ void nmmtl_dc_resistance(double the_conductivity,
 
   j = 0;
   item = signals;
-  for (item = signals; item != NULL; item = item->next)
-    {
-      if ( item->conductivity != 0.0 )
-  conductivity = item->conductivity;
+  for (item = signals; item != NULL; item = item->next) {
+    if ( item->conductivity != 0.0 )
+      conductivity = item->conductivity;
 
-      switch (item->primitive)
-  {
-  case RECTANGLE:
-    area = (item->x1 - item->x0) * (item->y1 - item->y0);
-    break;
-  case CIRCLE:
-    area = PI * item->x1 * item->x1;
-    break;
-  case POLYGON:
-    area = nmmtlPolyArea(item);
-    break;
-  }
-      Rdc[j][j] = 1.0 / (conductivity * area);
-      j++;
+    switch (item->primitive) {
+    case RECTANGLE:
+      area = (item->x1 - item->x0) * (item->y1 - item->y0);
+      break;
+    case CIRCLE:
+      area = PI * item->x1 * item->x1;
+      break;
+    case POLYGON:
+      area = nmmtlPolyArea(item);
+      break;
+    default:
+      printf("ERROR: primitive of item is unknown.");
+      return;
     }
+    Rdc[j][j] = 1.0 / (conductivity * area);
+    j++;
+  }
 
   // write it out //
-
   if (fp1 != NULL)
     output_fp =fp1;
   else
     output_fp = fp2;
-  while (output_fp != 0)
-    {
-      /********************************************************************
-       *                                                                   *
-       * Output Rdc matrix [Rdc].         *
-       *                                                                   *
-       ********************************************************************/
-      fprintf(output_fp, "\nRdc:\n");
-      fprintf(output_fp, "Rdc(Active Signal , Passive Signal) Ohms/Meter\n");
-      for (sig_line1 = signals, i = 0;
-     sig_line1 != NULL;
-     sig_line1 = sig_line1->next, i++)
-  {
-    for (sig_line2 = signals, j = 0;
-         sig_line2 != NULL;
-         sig_line2 = sig_line2->next, j++)
-      {
+
+  while (output_fp != 0) {
+    /********************************************************************
+     *                                                                   *
+     * Output Rdc matrix [Rdc].         *
+     *                                                                   *
+     ********************************************************************/
+    fprintf(output_fp, "\nRdc:\n");
+    fprintf(output_fp, "Rdc(Active Signal , Passive Signal) Ohms/Meter\n");
+    for (sig_line1 = signals, i = 0;
+         sig_line1 != NULL;
+         sig_line1 = sig_line1->next, i++) {
+      for (sig_line2 = signals, j = 0;
+           sig_line2 != NULL;
+           sig_line2 = sig_line2->next, j++) {
         fprintf(output_fp, "Rdc( ::%s , ::%s )= %15.7e\n",
-          sig_line1->name, sig_line2->name, Rdc[i][j]);
+        sig_line1->name, sig_line2->name, Rdc[i][j]);
       }
-  }
-      if (output_fp != fp2)
-  output_fp = fp2;
-      else
-  output_fp = 0;
+    }
+    if (output_fp != fp2)
+      output_fp = fp2;
+    else
+      output_fp = 0;
     }
 }
